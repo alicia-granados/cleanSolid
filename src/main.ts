a0ccee2ab2f725cb4639283aa4db6177ade497ee
alicia-granados <aliciagranados35@gmail.com>
2024-02-23 15:16:19 -0600

    feat: :sparkles: ejemplo de pricipio open y close OCP

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './style.css'
 //import './clean-code/07-tarea'
 //import './code-smells/02-high-coupling'
-import './solid/01-srp'
-
+//import './solid/01-srp'
+import './solid/02-openCloseA'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
